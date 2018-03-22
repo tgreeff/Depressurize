668c94e6676baf33d629ad9db2122aefb01a239c
masantac <marcel.santacruz@asu.edu>
2018-03-22 13:09:54 -0700

    basic UI

---
+++ Assets/scripts/timerTwo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerTwo : MonoBehaviour {
+
+    Image fillImg;
+    float timeAmt = 10;
+    float time; 
+
+	// Use this for initialization
+	void Start () {
+        fillImg = this.GetComponent<Image>();
+        time = timeAmt;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (time > 1)
+        {
+            time -= Time.deltaTime;
+            fillImg.fillAmount = time / timeAmt; // 9/10, 8/10, 7/10 ....
+        }
+	}
+}
