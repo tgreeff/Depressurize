c19a0eb809bbb0991fb9dd7829e086dd6f1a6880
masantac <marcel.santacruz@asu.edu>
2018-04-03 12:26:28 -0700

    main menu

---
+++ Assets/scripts/UI_Hover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_Hover : MonoBehaviour
+{
+
+    public GameObject myPanel;
+   
+
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject(-1))
+        {
+            myPanel.SetActive(true);
+        }
+
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            myPanel.SetActive(false);
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        myPanel.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        myPanel.SetActive(false);
+    }
+}
